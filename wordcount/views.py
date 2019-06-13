a1ff66e7ef5339478d6c5c29d2c79c47e0445d0b
Joseph <joewesaala@gmail.com>
2019-06-13 15:34:10 +0300

    Sort occurrence of  words from most to list

--- wordcount/views.py
+++ wordcount/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import operator
 
 
 def homepage(request):
@@ -23,4 +24,6 @@ def count(request):
             #add to the dictionary
             worddictionary[word] = 1
 
-    return render(request, 'count.html', {'fulltext': fulltext, 'count': len(wordlist), 'worddictionary': worddictionary.items()})
+    sortedwords = sorted(worddictionary.items(), key=operator.itemgetter(1), reverse=True)
+
+    return render(request, 'count.html', {'fulltext': fulltext, 'count': len(wordlist), 'sortedwords': sortedwords})
